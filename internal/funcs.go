4c69c742e283f344d748ac353f0352b0bb5e558f
K <x00.x7f@gmail.com>
2018-07-23 11:47:01 +0900

    Added http://masterminds.github.io/sprig/ functions

--- internal/funcs.go
+++ internal/funcs.go
@@ -5,14 +5,15 @@ import (
 	"strings"
 	"text/template"
 
+	"github.com/Masterminds/sprig"
 	"github.com/kenshaw/snaker"
-
 	"github.com/xo/xo/models"
 )
 
 // NewTemplateFuncs returns a set of template funcs bound to the supplied args.
 func (a *ArgType) NewTemplateFuncs() template.FuncMap {
-	return template.FuncMap{
+	fkmap := template.FuncMap(sprig.GenericFuncMap())
+	tmp := template.FuncMap{
 		"colcount":           a.colcount,
 		"colnames":           a.colnames,
 		"colnamesmulti":      a.colnamesmulti,
@@ -34,6 +35,10 @@ func (a *ArgType) NewTemplateFuncs() template.FuncMap {
 		"hasfield":           a.hasfield,
 		"getstartcount":      a.getstartcount,
 	}
+	for k, v := range tmp {
+		fkmap[k] = v
+	}
+	return fkmap
 }
 
 // retype checks typ against known types, and prefixing
