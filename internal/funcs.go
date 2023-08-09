9aa4d14d1a581aa349c7b642902459bb85baf4a7
syumai <syumai@gmail.com>
2023-08-09 18:21:20 +0900

    add fieldsnamelist func

--- internal/funcs.go
+++ internal/funcs.go
@@ -24,6 +24,7 @@ func (a *ArgType) NewTemplateFuncs() template.FuncMap {
 		"colvals":            a.colvals,
 		"colvalsmulti":       a.colvalsmulti,
 		"fieldnames":         a.fieldnames,
+		"fieldsnamelist":     a.fieldsnamelist,
 		"fieldToLowerNames":  a.fieldToLowerNames,
 		"fieldgetternames":   a.fieldgetternames,
 		"fieldnamesmulti":    a.fieldnamesmulti,
@@ -428,6 +429,22 @@ func (a *ArgType) fieldnames(fields []*Field, prefix string, ignoreNames ...stri
 	}
 
 	return str
+}
+
+func (a *ArgType) fieldsnamelist(fields []*Field, ignoreNames ...string) []string {
+	ignore := map[string]bool{}
+	for _, n := range ignoreNames {
+		ignore[n] = true
+	}
+
+	list := make([]string, 0, len(fields))
+	for _, f := range fields {
+		if ignore[f.Name] {
+			continue
+		}
+		list = append(list, f.Name)
+	}
+	return list
 }
 
 func (a *ArgType) fieldToLowerNames(fields []*Field, prefix string, ignoreNames ...string) string {
